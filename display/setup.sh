7880787adb5bba74ed83b9746eaf745e019a21dd
Antoine Andrieu <antoine@scircula.com>
2020-07-17 16:30:21 +0200

    Display setup with two monitors

--- display/setup.sh
+++ display/setup.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-intern=eDP-1
-extern=DP-2-2
 
-if xrandr | grep "$extern disconnected"; then
-    xrandr --output "$extern" --off --output "$intern" --auto
-else
-    xrandr --output "$intern" --off --output "$extern" --primary --mode 3440x1440 --pos 0x0 --rotate normal
-fi
+xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2-2 --primary --mode 3440x1440 --pos 1920x0 --rotate normal
